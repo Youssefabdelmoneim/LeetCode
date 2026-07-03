fcadef3a4a5b56d79f6b2d287132b3ffdfd9c596
Youssef abdelmonem <138399955+Youssefabdelmoneim@users.noreply.github.com>
2026-07-03 20:36:06 +0300

    Time: 246 ms (9.44%), Space: 88.1 MB (5%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -17,7 +17,34 @@ public:
   }
 
   string hashing(string s)
+  {class Solution
+{
+public:
+  bool checkInclusion(string s1, string s2)
   {
+
+    int len = s1.length();
+    vector<int> tmp1(26, 0);
+    vector<int> tmp2(26, 0);
+
+    for (auto ch : s1)
+      tmp1[ch - 'a']++;
+    int l = 0;
+    for (int i = 0; i < len && i < s2.length(); i++)
+    {
+      tmp2[i - 'a']++;
+    }
+
+    if (tmp1 == tmp2)
+      for (int r = len; r < s2.length(); r++)
+      {
+        if (tmp1 == tmp2)
+          tmp2[l++]--;
+        tmp2[r - 'a']++;
+      }
+    return tmp1 == tmp2;
+  }
+};
     vector<int> vec(26, 0);
     string hashed = "";
     for (auto ch : s)
